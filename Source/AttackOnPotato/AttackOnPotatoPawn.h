f6dab009810c3e03ed1ebf8e906584ae143f281c
lonerider543 <dominic.mathuse@googlemail.com>
2017-02-25 10:51:22 +0000

    Removed firing mechanics

--- Source/AttackOnPotato/AttackOnPotatoPawn.h
+++ Source/AttackOnPotato/AttackOnPotatoPawn.h
@@ -25,20 +25,20 @@ public:
 	AAttackOnPotatoPawn();
 
 	/** Offset from the ships location to spawn projectiles */
-	UPROPERTY(Category = Gameplay, EditAnywhere, BlueprintReadWrite )
-	FVector GunOffset;
+	// UPROPERTY(Category = Gameplay, EditAnywhere, BlueprintReadWrite )
+	// FVector GunOffset;
 	
 	/* How fast the weapon will fire */
-	UPROPERTY(Category = Gameplay, EditAnywhere, BlueprintReadWrite)
-	float FireRate;
+	// UPROPERTY(Category = Gameplay, EditAnywhere, BlueprintReadWrite)
+	// float FireRate;
 
 	/* The speed our ship moves around the level */
 	UPROPERTY(Category = Gameplay, EditAnywhere, BlueprintReadWrite)
 	float MoveSpeed;
 
 	/** Sound to play each time we fire */
-	UPROPERTY(Category = Audio, EditAnywhere, BlueprintReadWrite)
-	class USoundBase* FireSound;
+	// UPROPERTY(Category = Audio, EditAnywhere, BlueprintReadWrite)
+	// class USoundBase* FireSound;
 
 	// Begin Actor Interface
 	virtual void Tick(float DeltaSeconds) override;
@@ -46,24 +46,24 @@ public:
 	// End Actor Interface
 
 	/* Fire a shot in the specified direction */
-	void FireShot(FVector FireDirection);
+	// void FireShot(FVector FireDirection);
 
 	/* Handler for the fire timer expiry */
-	void ShotTimerExpired();
+	// void ShotTimerExpired();
 
 	// Static names for axis bindings
 	static const FName MoveForwardBinding;
 	static const FName MoveRightBinding;
-	static const FName FireForwardBinding;
-	static const FName FireRightBinding;
+	// static const FName FireForwardBinding;
+	// static const FName FireRightBinding;
 
 private:
 
 	/* Flag to control firing  */
-	uint32 bCanFire : 1;
+	// uint32 bCanFire : 1;
 
 	/** Handle for efficient management of ShotTimerExpired timer */
-	FTimerHandle TimerHandle_ShotTimerExpired;
+	// FTimerHandle TimerHandle_ShotTimerExpired;
 
 public:
 	/** Returns ShipMeshComponent subobject **/
