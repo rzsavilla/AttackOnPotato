8beb17087b868bc77a87398e34a907daf5adcaa5
lonerider543 <dominic.mathuse@googlemail.com>
2017-02-26 22:11:06 +0000

    Minor Fixes

--- Source/AttackOnPotato/AttackOnPotatoGameMode.cpp
+++ Source/AttackOnPotato/AttackOnPotatoGameMode.cpp
@@ -6,11 +6,6 @@
 
 AAttackOnPotatoGameMode::AAttackOnPotatoGameMode()
 {
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/AttackOnPotato/Blueprints/BP_AttackOnPotatoCharacter.BP_AttackOnPotatoCharacter"));
-	if (PlayerPawnBPClass.Class != NULL)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
+
 }
 
